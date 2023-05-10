48d8f1f01e87fdab192e0da7c5a1c21856d0b589
Paola Mangelli <mangelli.paola90@gmail.com>
2023-05-10 19:36:13 -0400

    commit changes

--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -1,35 +1,38 @@
 import React from "react";
 
 const NavBar = () => {
-	return ( 
-<>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-        <a className="navbar-brand" href="#">Start Bootstrap</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-            <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href="#">Services</a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href="#">Contact</a>
-            </li>
-        </ul>
-        </div>
-    </div>
-    </nav>
-</>
-)
+    return (
+        <>
+            <div className="container-fluid bg-dark  ">
+                <div className="container bg-dark  ">
+                    <div className="row">
+                        <nav className="navbar navbar-dark bg-dark d-flex ">
+                            <div className="container-fluid d-flex">
+                                <a className="navbar-brand " href="#">Start Bootstrap</a>
+                                <div className="justify-content-end">
+                                    <ul className="nav">
+                                        <li className="nav-item">
+                                            <a className="nav-link" href="#">Home</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link" href="#">About</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link" href="#">Services</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link ">Contact</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                        </nav>
+                    </div>
+                </div >
+            </div>
+        </>
+    )
 };
 
 export default NavBar;
--- src/js/page/Home.jsx
+++ src/js/page/Home.jsx
@@ -5,24 +5,57 @@ const Home = () => {
     return (
         <>
             <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-8 bg-secondary">
+                <div className="row justify-content-center ">
+                    <div className="col-12 bg-secondary pb-5  ">
                         <h1>A Warm Welcome!</h1>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                            Distinctio nisi consequatur dolores repellendus, similique
-                            ipsa dolorum qui quod repellat rem.
-                        </p>
-                        <button type="button" class="btn btn-primary">Call to Action!</button>
+                        <br/>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio excepturi 
+                            culpa alias laboriosam exercitationem accusantium consequuntur possimus 
+                            magnam adipisci tenetur aliquid tempore fugiat earum, nihil cumque eligendi 
+                            omnis in distinctio repellat voluptatem consequatur vel amet. Id recusandae 
+                            non ratione corrupti.
+                        </p> <br/>
+                        <button type="button" class="btn btn-primary">Call to Action!</button> 
                     </div>
                 </div>
-                <div className="row justify-content-center">
-                    <div className="col-8">
-                        <div className="card">
-                            <img src="..." class="card-img-top" alt="..." />
+                <div className="row justify-content-center mt-3">
+                    <div className="col-3 ">
+                        <div className="card text-center ">
+                            <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body ">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Find out more!</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card text-center">
+                            <img src="..." className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                                <a href="#" className="btn btn-primary">Find out more!</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card text-center">
+                            <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Find out more!</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card text-center">
+                            <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Find out more!</a>
                             </div>
                         </div>
                     </div>
